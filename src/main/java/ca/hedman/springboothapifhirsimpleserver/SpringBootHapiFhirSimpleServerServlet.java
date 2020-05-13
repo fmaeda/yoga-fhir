c8e7d4d3f17b154df2db65e2b495971619f5db0a
bhedman <bruno.hedman@rmp.uhn.ca>
2020-05-13 13:41:36 -0400

    removed unnecessary constructor and env.

--- src/main/java/ca/hedman/springboothapifhirsimpleserver/SpringBootHapiFhirSimpleServerServlet.java
+++ src/main/java/ca/hedman/springboothapifhirsimpleserver/SpringBootHapiFhirSimpleServerServlet.java
@@ -4,28 +4,16 @@ import ca.hedman.springboothapifhirsimpleserver.provider.PatientProvider;
 import ca.uhn.fhir.rest.api.EncodingEnum;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.RestfulServer;
-import ca.uhn.fhir.rest.server.interceptor.CorsInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.ResponseHighlighterInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.web.cors.CorsConfiguration;
 
 import javax.servlet.annotation.WebServlet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/fhir/*", loadOnStartup = 1)
 public class SpringBootHapiFhirSimpleServerServlet extends RestfulServer {
 
     private static final long serialVersionUID = 1L;
-
-    private Environment env;
-
-    @Autowired
-    SpringBootHapiFhirSimpleServerServlet(Environment environment) {
-        this.env = environment;
-    }
 
     @Override
     public void initialize() {
