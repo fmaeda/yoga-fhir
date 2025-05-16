834ecd13ca522da5a4d1166b1ffb034951135982
Fabiano Maeda <fabianomaeda@gmail.com>
2025-05-16 10:26:38 -0300

    Merged test

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-# regex to validate in commit msg
-# https://regex101.com/r/VTEL5G/3
 commit_regex='(Merge(.*)|([A-Z_]+-[0-9]+)|((\d+\.)?(\d+\.)?(\d+).(RELEASE|SNAPSHOT)))'
 error_msg="Mensagem de commit deve iniciar com o número da issue JIRA (ex. 'MME-xxx', 'INC-xxx'), 'Merge(d)'"
 echo "#!/usr/bin/env bash
-
 # Part 1
 stagedFiles=$(git diff --staged --name-only)
+
 # Part 2
 echo \"Running spotlessApply. Formatting code...\"
 ./mvnw spotless:apply
+
 # Part 3
 for file in $stagedFiles; do
   if test -f \"$file\"; then
     git add $file
   fi
 done
+" > .git/hooks/pre-commit
 
+echo "#!/usr/bin/env bash
 commit_regex='$commit_regex'
 error_msg=\"$error_msg\"
 if ! grep -qE \"\$commit_regex\" \"\$1\"; then
@@ -27,5 +28,4 @@ fi
 " > .git/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
 chmod +x mvnw
-#rm .git/hooks/commit-msg.sample
 echo "Setup do Script do pre-commit hook finalizado!"
